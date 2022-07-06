308f8b4ea6fac1c5751973ad055e2a0fc2d782f8
Konstantin Komovits <konsta.2106@gmail.com>
2022-07-06 13:19:58 +0300

    updates

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+console.log(process.env.NEXT_PUBLIC_TEST_ENV)
+console.log(process.env.DIRECT_ENV)
+
 export default function Home() {
   return (
     <div className={styles.container}>
